b917013dc6936c5ddb8f6961ea8ce2e8cee5445b
andrei-kryvetski <kryvetskistud@gmail.com>
2021-09-07 22:46:27 +0300

    feat: complite st-common-character-count subtask

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,35 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function getCommonCharacterCount(s1, s2) {
+
+  const creatObjWithCountOfUniqueValues = (str) => {
+    const splitedStr = str.split('');
+    const obj = {};
+
+    splitedStr.forEach(char => {
+      if (!obj[char]) obj[char] = 0;
+      obj[char] += 1;
+    })
+    return obj;
+  }
+
+  const firstObject = creatObjWithCountOfUniqueValues(s1);
+  const secondObject = creatObjWithCountOfUniqueValues(s2);
+
+  const compareCounts = (firstObj, secondObj) => {
+    let count = 0;
+
+    for (let keyOfFirstObj in firstObj) {
+      for (let keyOfSecondObj in secondObj) {
+        if (keyOfFirstObj === keyOfSecondObj) {
+          count += Math.min(firstObj[keyOfFirstObj], secondObj[keyOfSecondObj]);
+        };
+      };
+    };
+
+    return count;
+  };
+  
+  return compareCounts(firstObject, secondObject);
+};
