93b96672ae6303b47f720ec99180f64aa8542d58
andrei-kryvetski <kryvetskistud@gmail.com>
2021-09-07 20:29:42 +0300

    feat: complite dream-team subtask

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function createDreamTeam(members) {
+  const dreamTeamName = [];
+
+  if (Array.isArray(members)) {
+    members.forEach(memberName => {
+      if (typeof memberName === 'string') dreamTeamName.push(memberName.trim()[0].toUpperCase());
+    });
+  };
+
+  return dreamTeamName.sort().join('');
+};
