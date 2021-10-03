4871d3afabb76368426157ad2a1ac87548f34819
kryvetski-andrei <kryvetskistud@gmail.com>
2021-10-03 21:54:51 +0300

    feat: add st-encode-line solution

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,48 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  if (str.length === 0) return '';
+
+  const arr = str.split('');
+  let stack = [];
+  let result = '';
+
+  arr.forEach((letter) => {
+    if (stack.length > 0) {
+      if (letter === stack[stack.length - 1]) stack.push(letter);
+      else {
+        stack.length === 1
+          ? (result += stack[0])
+          : (result += `${stack.length}${stack[0]}`);
+        stack = [];
+      }
+    }
+
+    if (stack.length === 0) stack.push(letter);
+  });
+
+  stack.length === 1
+    ? (result += stack[0])
+    : (result += `${stack.length}${stack[0]}`);
+
+  return result;
+
+  // const arr = str.split('');
+  // let collectedStr = '';
+
+  // const obj = arr.reduce((count, letter) => {
+
+  //   count[letter] = (count[letter] || 0) + 1;
+  //   // console.log(count[letter], 'count letter')
+  //   // console.log(count)
+  //   return count;
+  // }, {})
+
+  // console.log(obj)
+
+  // for (let letter in obj) {
+  //   collectedStr += obj[letter] + letter;
+  // }
+  // return collectedStr;
 }
