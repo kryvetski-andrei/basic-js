f3444f14d2acf079c0f297556958d22759cc2c92
kryvetski-andrei <kryvetskistud@gmail.com>
2021-10-04 16:38:27 +0300

    feat: add solution st-sort-by-height

--- src/st-sum-digits.js
+++ src/st-sum-digits.js
@@ -12,7 +12,10 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-export default function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSumOfDigits(n) {
+  const arr = n.toString().split('').map(str => +str);
+  const num = arr.reduce((result, curr) => result + curr);
+
+  if (num > 9) return getSumOfDigits(num);
+  return num;
 }
