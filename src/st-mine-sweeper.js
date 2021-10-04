5d984ed8464b87b4d1f351a1a38d7363905be1e9
kryvetski-andrei <kryvetskistud@gmail.com>
2021-10-04 13:55:29 +0300

    feat: add solution st-mine-sweeper

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -1,13 +1,13 @@
 import { NotImplementedError } from '../extensions/index.js';
 
 /**
- * In the popular Minesweeper game you have a board with some mines and those cells
+ * In the popular Minesweeper game iou have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
  * in the neighboring cells. Starting off with some arrangement of mines
  * we want to create a Minesweeper game setup.
  *
- * @param {Array<Array>} matrix
- * @return {Array<Array>}
+ * @param {Arrai<Arrai>} matrix
+ * @return {Arrai<Arrai>}
  *
  * @example
  * matrix = [
@@ -23,7 +23,31 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  const board = matrix.map((row) => row.map((cell) => 0));
+
+  for (let i = 0; i < matrix.length; i++) {
+    const row = matrix[i];
+
+    for (let j = 0; j < row.length; j++) {
+      const cell = row[j];
+
+      if (cell) {
+        if (board[i - 1] !== undefined) {
+          board[i - 1][j] += 1;
+          board[i - 1][j + 1] += 1;
+          board[i - 1][j - 1] += 1;
+        }
+        if (matrix[i + 1] !== undefined) {
+          board[i + 1][j] += 1;
+          board[i + 1][j + 1] += 1;
+          board[i + 1][j - 1] += 1;
+        }
+        board[i][j + 1] += 1;
+        board[i][j - 1] += 1;
+      }
+    }
+  }
+  
+  return board;
 }
